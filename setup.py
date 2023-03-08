8ce59c50cae163422e42f2704116077f0cc62f1f
Jong Wook Kim <jongwook@openai.com>
2023-03-07 19:56:31 -0500

    Use triton==2.0.0 (#1053)

--- setup.py
+++ setup.py
@@ -14,21 +14,7 @@ with open(os.path.join(HERE, "README.md"), encoding="utf-8", mode="r") as fd:
 
 requirements = []
 if sys.platform.startswith("linux") and platform.machine() == "x86_64":
-    triton_requirement = "triton==2.0.0"
-    try:
-        import re
-        import subprocess
-
-        version_line = (
-            subprocess.check_output(["nvcc", "--version"]).strip().split(b"\n")[-1]
-        )
-        major, minor = re.findall(rb"([\d]+)\.([\d]+)", version_line)[0]
-        if (int(major), int(minor)) < (11, 4):
-            # the last version supporting CUDA < 11.4
-            triton_requirement = "triton==2.0.0.dev20221011"
-    except (IndexError, OSError, subprocess.SubprocessError):
-        pass
-    requirements.append(triton_requirement)
+    requirements.append("triton==2.0.0")
 
 setup(
     name="whisper",
